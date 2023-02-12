eb1f06cb4e33a8b020fcb547330e8521301ff618
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2023-02-12 19:23:28 +0300

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Dog
-
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
 end
+
+bruno = Dog.new
+bruno.name = "bruno"
+bruno.breed = "pug"
+binding.pry
